6ce60f93b1288d5c3b0736988051a4f9feb2c1c6
JoseQuesada94 <joseque0209@gmail.com>
2025-02-11 11:31:37 -0800

    Create boolean-conditional.js

---
+++ boolean-conditional.js
@@ -0,0 +1,10 @@
+//title: Training JS #6: Basic data types--Boolean and conditional statements if..else
+//https://www.codewars.com/kata/571f832f07363d295d001ba8/train/javascript
+
+function trueOrFalse(val){
+    if (Boolean(val))    
+      return "true";             
+    else     
+      return "false";
+  }
+  
