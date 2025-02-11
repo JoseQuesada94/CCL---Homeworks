b671de4aa97d633cc3415539484d26ac1ca1d3f5
JoseQuesada94 <joseque0209@gmail.com>
2025-02-11 11:03:37 -0800

    Create first-function.js

---
+++ first-function.js
@@ -0,0 +1,8 @@
+//Tittle: Training JS #1: create your first JS function and print "Hello World!"
+//https://www.codewars.com/kata/571ec274b1c8d4a61c0000c8/train/javascript
+
+
+function helloWorld(){
+  var str="Hello World"
+  console.log(str)
+}
